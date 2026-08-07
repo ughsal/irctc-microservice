990ca4424b8581d280c49890c9c96a324b805454
ughsal <salmansaiyed008@gmail.com>
2026-08-07 18:58:29 +0530

    fixed file path

---
+++ notification-service/src/config/kafka.ts
@@ -0,0 +1,47 @@
+import { Kafka, logLevel } from "kafkajs";
+import logger from "./logger";
+import config from "./";
+
+const kafka = new Kafka({
+  clientId: config.KAFKA_CLIENT_ID ?? "notification-service",
+  brokers: [config.KAFKA_BROKER ?? "localhost:9093"],
+  logLevel: logLevel.ERROR,
+  retry: {
+    initialRetryTime: 300,
+    retries: 8,
+    maxRetryTime: 3000,
+  },
+});
+
+const producer = kafka.producer({
+  allowAutoTopicCreation: true,
+  transactionTimeout: 30000,
+  idempotent: true,
+  maxInFlightRequests: 1,
+  retry: {
+    retries: 5,
+  },
+});
+
+let isConnected = false;
+
+const connectProducer = async () => {
+  if (!isConnected) {
+    await producer.connect();
+    isConnected = true;
+    logger.info("Kafka producer connected");
+  }
+};
+
+const disconnectProducer = async () => {
+  if (isConnected) {
+    await producer.disconnect();
+    isConnected = false;
+    logger.info("Kafka producer disconnected");
+  }
+};
+
+process.on("SIGTERM", disconnectProducer);
+process.on("SIGINT", disconnectProducer);
+
+export { producer, connectProducer, disconnectProducer };
