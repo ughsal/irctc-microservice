0ad2be57d0965350e57176d21d5db898f5c66140
ughsal <salmansaiyed008@gmail.com>
2026-08-07 18:38:44 +0530

    Added kafka producer functions

--- user-service/src/config/kafka.ts
+++ user-service/src/config/kafka.ts
@@ -12,3 +12,33 @@ const kafka = new Kafka({
     maxRetryTime: 3000,
   },
 });
+
+const producer = kafka.producer({
+  allowAutoTopicCreation: true,
+  transactionTimeout: 30000,
+  idempotent: true,
+  maxInFlightRequests: 1,
+  retry: {
+    retries: 5,
+  },
+});
+let isConnected = false;
+const connectProducer = async () => {
+  if (!isConnected) {
+    await producer.connect();
+    isConnected = true;
+    logger.info("Kafka producer connected");
+  }
+};
+const disconnectProducer = async () => {
+  if (isConnected) {
+    await producer.disconnect();
+    isConnected = false;
+    logger.info("Kafka producer disconnected");
+  }
+};
+
+process.on("SIGTERM", disconnectProducer);
+process.on("SIGINT", disconnectProducer);
+
+export { producer, connectProducer, disconnectProducer };
